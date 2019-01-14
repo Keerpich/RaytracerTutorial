b450d1a7ca9ce89119ee01e28962fabfc36c4e87
Daniel Alexandru Radu <danyhk94@gmail.com>
2019-01-14 14:51:54 +0200

    Create Ray class

---
+++ RaytracerTutorial/Raytracer/include/Ray.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Vec3.h"
+
+class Ray
+{
+public:
+	Ray() = default;
+	Ray(const Vec3 &origin, const vec3 &direction) :
+		mOrigin(origin), mDirection(direction) {}
+
+	~Ray() = default;
+
+	inline Vec3 origin() const { return mOrigin; }
+	inline Vec3 direction() const { return mDirection; }
+	inline Vec3 point_at_parameter(float t) const { return mOrigin + t * mDirection; }
+
+private:
+	Vec3 mOrigin;
+	Vec3 mDirection;
+};
